f73855eb4522fbabb4395d1859e3f4ad77b8726e
DarrylCully <Dazmufc93@gmail.com>
2022-01-10 19:48:13 +0000

    Added Title Scene
    Added title scene

---
+++ BogoVr/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // load level 1
+    public GameObject Player;
+    public void LoadGame()
+    {
+        Player.transform.position = new Vector3(0, 0, 0);
+        SceneManager.LoadScene("SampleScene");
+        Debug.Log("Load Level 1");
+    }
+
+    public void LoadTutorial()
+    {
+        Player.transform.position = new Vector3(0, 0, 0);
+        SceneManager.LoadScene("Tutorial");
+        Debug.Log("Tutorial");
+    }
+    //quit the application
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
